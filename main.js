2c77d34c0d1d3ad1b6ae53e6a6e9f442ac6acef9
Amir <aassis@mail.austral.edu.ar>
2024-08-28 23:58:00 -0300

    cambios agregados

--- main.js
+++ main.js
@@ -1,22 +1,32 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const sliderItems = document.querySelectorAll(".slider-item");
-  let currentIndex = 0;
+  // Funciones para un slider
+  function setupSlider(sliderId, prevButtonId, nextButtonId) {
+    const sliderItems = document.querySelectorAll(`#${sliderId} .slider-item`);
+    let currentIndex = 0;
 
-  function showSlide(index) {
-    sliderItems.forEach((item) => item.classList.remove("active"));
-    sliderItems[index].classList.add("active");
+    function showSlide(index) {
+      sliderItems.forEach((item) => item.classList.remove("active"));
+      sliderItems[index].classList.add("active");
+    }
+
+    function nextSlide() {
+      currentIndex = (currentIndex + 1) % sliderItems.length;
+      showSlide(currentIndex);
+    }
+
+    function prevSlide() {
+      currentIndex =
+        (currentIndex - 1 + sliderItems.length) % sliderItems.length;
+      showSlide(currentIndex);
+    }
+
+    document.getElementById(nextButtonId).addEventListener("click", nextSlide);
+    document.getElementById(prevButtonId).addEventListener("click", prevSlide);
   }
 
-  function nextSlide() {
-    currentIndex = (currentIndex + 1) % sliderItems.length;
-    showSlide(currentIndex);
-  }
+  // Configura el primer slider
+  setupSlider("slider1", "prevButton1", "nextButton1");
 
-  function prevSlide() {
-    currentIndex = (currentIndex - 1 + sliderItems.length) % sliderItems.length;
-    showSlide(currentIndex);
-  }
-
-  document.getElementById("nextButton").addEventListener("click", nextSlide);
-  document.getElementById("prevButton").addEventListener("click", prevSlide);
+  // Configura el segundo slider
+  setupSlider("slider2", "prevButton2", "nextButton2");
 });
